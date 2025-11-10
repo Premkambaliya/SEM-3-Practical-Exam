2dabac54300ad592746e93942a9a85437cf43f89
Kambaliya Prem <prem.kambaliya.cg@gmail.com>
2025-11-10 14:21:37 +0530

    i add JAVA practicle Exam Question

---
+++ JAVA Exam/Q-1.java
@@ -0,0 +1,22 @@
+class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Sum of two integers: " + calc.add(1, 2));
+        System.out.println("Sum of three integers: " + calc.add(1, 2, 3));
+        System.out.println("Sum of two doubles: " + calc.add(1.5, 2.5));
+    }
+}
